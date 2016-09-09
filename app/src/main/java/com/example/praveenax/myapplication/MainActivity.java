25d6710256f0259e2e2338d9eeb4ac94563a4869
praveenax <xav.axp@gmail.com>
2016-09-09 23:28:34 +0530

    added util alert messages

--- app/src/main/java/com/example/praveenax/myapplication/MainActivity.java
+++ app/src/main/java/com/example/praveenax/myapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.praveenax.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
 
     // Array of strings...
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
@@ -15,9 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button new_note = (Button)findViewById(R.id.button);
+        new_note.setOnClickListener(this);
+
+
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.act_listview, mobileArray);
 
         ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(this);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1,
+                            int position, long id) {
+
+//        Intent in1 = new Intent(Activites_Activity.this, Activity_display.class);
+//
+//        startActivity(in1);
+
+        Toast.makeText(getApplicationContext(),
+                "Button is clicked "+mobileArray[position], Toast.LENGTH_SHORT).show();
+
+        Util.showHelp("Objective: To turn off all the Lights with the least number of moves \n" +
+                "\n" +
+                "Each switch is inter-connected with other lights in a perpendicular fashion",this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Util.showHelp("TEST NOTE \n" +
+                "\n" +
+                "Each switch is inter-connected with other lights in a perpendicular fashion",this);
+
     }
 }
---
+++ app/src/main/java/com/example/praveenax/myapplication/Util.java
@@ -0,0 +1,44 @@
+package com.example.praveenax.myapplication;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+/**
+ * Created by Praveenax on 9/9/2016.
+ */
+public class Util {
+
+    public static void showAlert(String message,Context context) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set title
+        alertDialogBuilder.setTitle("Result");
+
+        // set dialog message
+        alertDialogBuilder.setMessage(message);
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public static void showHelp(String message,Context context) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set title
+        alertDialogBuilder.setTitle("How to Play!");
+
+        // set dialog message
+        alertDialogBuilder.setMessage(message);
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+}
