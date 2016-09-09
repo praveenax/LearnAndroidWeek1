02a170bd150650dc6aa78e2ec63bf97c59b3a7c5
praveenax <xav.axp@gmail.com>
2016-09-10 00:22:29 +0530

    Added list update on new note

--- app/src/main/java/com/example/praveenax/myapplication/MainActivity.java
+++ app/src/main/java/com/example/praveenax/myapplication/MainActivity.java
@@ -3,17 +3,27 @@ package com.example.praveenax.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.content.DialogInterface;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
 
     // Array of strings...
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+
+    private EditText result;
+    ArrayList<String> mobArray;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Button new_note = (Button)findViewById(R.id.button);
         new_note.setOnClickListener(this);
 
+        mobArray = new ArrayList<String>();
+        mobArray.add("Android");
+        mobArray.add("IPhone");
+        mobArray.add("WindowsMobile");
+        mobArray.add("Blackberry");
+        mobArray.add("WebOS");
+        mobArray.add("Ubuntu");
+        mobArray.add("Windows7");
+        mobArray.add("Max OS X");
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.act_listview, mobileArray);
+        adapter = new ArrayAdapter<String>(this, R.layout.act_listview, mobArray);
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setOnItemClickListener(this);
@@ -40,9 +59,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 //        startActivity(in1);
 
         Toast.makeText(getApplicationContext(),
-                "Button is clicked "+mobileArray[position], Toast.LENGTH_SHORT).show();
+//                "Button is clicked "+mobileArray[position], Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(),
+                "Button is clicked "+mobArray.get(position) , Toast.LENGTH_SHORT).show();
 
-        Util.showHelp("OS Name!",""+mobileArray[position]+" is Selected",this);
+//        Util.showHelp("OS Name!",""+mobileArray[position]+" is Selected",this);
+        Util.showHelp("OS Name!",""+mobArray.get(position)+" is Selected",this);
 
 
 
@@ -50,7 +72,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onClick(View view) {
-        Util.showHelp("Add New Note","TEST NOTE",this);
+//        Util.showHelp("Add New Note","TEST NOTE",this);
+
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompts, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+//                                result.setText(userInput.getText());
+
+                                mobArray.add(""+userInput.getText());
+
+                                adapter.notifyDataSetChanged();
+
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
 
     }
 }
